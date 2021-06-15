ef7a76745c9130b9f9e4b54f8b9613aee514a6b2
Coline Dusserre <coline.dusserre@kedgebs.com>
2021-06-15 13:47:31 +0200

    navbar

--- app/javascript/channels/consumer.js
+++ app/javascript/channels/consumer.js
@@ -2,5 +2,7 @@
 // You can generate new channels where WebSocket features live using the `rails generate channel` command.
 
 import { createConsumer } from "@rails/actioncable"
-
+import { navbar } from "./partials/header"
 export default createConsumer()
+
+navbar()
