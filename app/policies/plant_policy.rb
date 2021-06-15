649b94bc4110da94472c5f6bd17192c3e4dc7080
Julian Demeuse <juliandemeuse@gmail.com>
2021-06-15 11:37:09 +0200

    pundit done

---
+++ app/policies/plant_policy.rb
@@ -0,0 +1,19 @@
+class PlantPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    return true
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+end
