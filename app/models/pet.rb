e6a77d836cdde130f93c131bedb0e014d4b846f5
Coline Dusserre <coline.dusserre@kedgebs.com>
2021-06-16 17:29:15 +0200

    add a category to pets

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,13 +2,13 @@ class Pet < ApplicationRecord
   belongs_to :user
   has_many :categories, through: :pet_categories
   has_many :pet_notifications, through: :pet_categories
-  TYPE = %w[cat dog]
-  TYPE_FILTER = TYPE.map do |type|
-                        [type.capitalize, type]
+  SPECIES = %w[cat dog]
+  SPECIES_FILTER = SPECIES.map do |species|
+                        [species.capitalize, species]
                       end
   validates :name, presence: true
   validates :breed, presence: true
-  validates :type, inclusion: { in: TYPE }
+  validates :species, inclusion: { in: SPECIES }
   validates :birthdate, presence: true
   has_one_attached :image
 end
