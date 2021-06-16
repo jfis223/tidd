89bd3b03d18d7ff40394ebb03ab2461901c3dc5c
Joaquin Fischer <joaquin.fischer@gmail.com>
2021-06-16 17:29:27 +0200

    Finished for dogs and cats

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,10 +4,31 @@ class Pet < ApplicationRecord
   has_many :pet_notifications, through: :pet_categories
   TYPE = %w[cat dog]
   TYPE_FILTER = TYPE.map do |type|
-                        [type.capitalize, type]
-                      end
+                       [type.capitalize, type]
+                         end
+  DOG_BREED = []
+
+  dog_url = 'https://api.thedogapi.com/v1/breeds'
+  dog_breeds_string = RestClient.get(dog_url).body
+  dog_breeds = JSON.parse(dog_breeds_string)
+
+  dog_breeds.map do |dog|
+    DOG_BREED << dog["name"]
+  end
+
+  CAT_BREED = []
+
+  cat_url = 'https://api.thecatapi.com/v1/breeds'
+  cat_breeds_string = RestClient.get(cat_url).body
+  cat_breeds = JSON.parse(cat_breeds_string)
+
+  cat_breeds.map do |cat|
+    CAT_BREED << cat["name"]
+  end
+
+
   validates :name, presence: true
-  validates :breed, presence: true
+  validates :breed, inclusion: { in: DOG_BREED || CAT_BREED }
   validates :type, inclusion: { in: TYPE }
   validates :birthdate, presence: true
   has_one_attached :image
