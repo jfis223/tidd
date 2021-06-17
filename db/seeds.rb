d1f3eabc73391e2e469126d2699bd6c59dd7df22
Joaquin Fischer <joaquin.fischer@gmail.com>
2021-06-17 15:55:24 +0200

    Finished seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts = "Deleting all previous elements..."
+puts "Deleting all previous elements..."
 
-User.delete_all
 Category.delete_all
 PetCategory.delete_all
 PetNotification.delete_all
 Pet.delete_all
 Plant.delete_all
+User.delete_all
 
 
 user = User.create!(name: 'joaquin', email: 'email@gmail.com', password: 'password')
+
+
+puts 'Creating your pet...'
+
+pet1 = Pet.create!(name: 'Coline', species: 'cat', breed: "American Curl", weight: 2, birthdate: '10/01/2020', location: 'Madrid', user: user)
+
