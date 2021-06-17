531f2330db8a30729c87cf4f3c249e93ff5c99c9
Joaquin Fischer <joaquin.fischer@gmail.com>
2021-06-17 15:40:26 +0200

    Fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,39 +2,28 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("partials/header")
-require("typed")
-
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-
 // ----------------------------------------------------
 // Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
 // WRITE YOUR OWN JS STARTING FROM HERE 👇
 // ----------------------------------------------------
-
 // External imports
 import "bootstrap";
-
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
-
 import { formPetSelection } from '../components/form_pet';
-
 import { loadDynamicBannerText } from '../components/banner';
-
 document.addEventListener('turbolinks:load', () => {
-  loadDynamicBannerText();
+  // loadDynamicBannerText();
   formPetSelection();
 });
